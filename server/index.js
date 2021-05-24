c5dad3331bc51ae97790355c82b73fbfe86184eb
garsan84 <jgrsan84@gmail.com>
2021-05-24 20:18:13 +0200

    connected with stripe

--- server/index.js
+++ server/index.js
@@ -1,17 +1,35 @@
-const express = require('express')
-const stripe = require('stripe')
-const cors = require('cors')
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors({origin: 'http://localhost:3000'} ))
-app.use(express.json())
+const stripe = new Stripe("sk_test_51ItWL3FsljR1JBuog6ig7dLqGjX3uLu0YuPgGALKBc8kWvTtds9ChreHvURQAA3qaZEIToQka8eQhWDT5b6QRjAd00GJBHcwRp")
 
-app.post('/api/checkout', (req, res) => {
-    console.log(req.body)
-    res.send('received')
-})
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+  try {
+    const { id, amount } = req.body;
+
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "EUR",
+      description: "raspberry",
+      payment_method: id,
+      confirm: true,
+    });
+
+    console.log(payment);
+
+    res.send({ message: "Succesful payment" });
+  } catch {
+    console.log(error);
+    res.json({ message: error });
+  }
+});
 
 app.listen(3001, () => {
-    console.log('Server on port', 3001)
-})
+  console.log("Server on port", 3001);
+});
--- src/App.js
+++ src/App.js
@@ -1,66 +1,95 @@
-
-import { loadStripe } from '@stripe/stripe-js'
-import axios from 'axios';
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 import "bootswatch/dist/lux/bootstrap.min.css";
-import './App.css';
-import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import "./App.css";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
 
-const stripePromise = loadStripe("pk_test_51ItWL3FsljR1JBuoBjwzyQHxPKsYWuYb4gwt9UpT7oPCWwNnPyOs1omRSRlmbshqsCZGE5KHR3f6trjFayWgSBzI00ZtZmYGLt")
+const stripePromise = loadStripe(
+  "pk_test_51ItWL3FsljR1JBuoBjwzyQHxPKsYWuYb4gwt9UpT7oPCWwNnPyOs1omRSRlmbshqsCZGE5KHR3f6trjFayWgSBzI00ZtZmYGLt"
+);
 
 const CheckoutForm = () => {
-
   const stripe = useStripe();
   const elements = useElements();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const {error, paymentMethod} =await stripe.createPaymentMethod({
-      type: 'card',
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
       card: elements.getElement(CardElement),
-    })
+    });
+    setLoading(true);
 
-    if (!error){
-       const { id } = paymentMethod;
+    if (!error) {
+      const { id } = paymentMethod;
 
-       const {data} = await axios.post('http://localhost:3001/api/checkout',{
-       id,
-       amount: 8890})
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            amount: 8890,
+          }
+        );
 
-       console.log(data)
+        console.log(data);
+        elements.getElement(CardElement).clear();
+      } catch (error) {
+        console.log(error);
+      }
+
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="card card-body">
       <div className="text-center">
-         <img src="https://img.pccomponentes.com/articles/29/297584/raspberry-pi-4-modelo-b-8gb.jpg" className="img-fluid" width="75%"/>
+        <img
+          src="https://img.pccomponentes.com/articles/29/297584/raspberry-pi-4-modelo-b-8gb.jpg"
+          className="img-fluid"
+          width="75%"
+        />
       </div>
-      <div className="description text-dark text-center" ><h5>Raspberry Pi 4 Modelo B 8GB</h5></div>
+      <div className="description text-dark text-center">
+        <h5>Raspberry Pi 4 Modelo B 8GB</h5>
+      </div>
       <h4 className="text-center">Price: 88,90€</h4>
 
-        <div className="form-group">
-         <CardElement className="form-control"/>
-        </div>
+      <div className="form-group">
+        <CardElement className="form-control" />
+      </div>
 
       <button className="btn btn-outline-secondary">
-        Pay
+        {loading ? (
+          <div className="spinner-border text-light" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          "Pay"
+        )}
       </button>
     </form>
-  )
-}
-
+  );
+};
 
 function App() {
   return (
-   <Elements stripe={stripePromise} >
-     <div className="container p-4">
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
         <div className="col-md-4 offset-md-4">
-         <CheckoutForm />
+          <CheckoutForm />
         </div>
-     </div>
-     
-   </Elements>
+      </div>
+    </Elements>
   );
 }
 

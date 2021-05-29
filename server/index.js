627a49e70d0a2bfb4296fdb60deb18df3d352f9d
garsan84 <jgrsan84@gmail.com>
2021-05-29 12:19:59 +0200

    videogame store and stripe

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-const stripe = new Stripe("sk_test_51ItWL3FsljR1JBuog6ig7dLqGjX3uLu0YuPgGALKBc8kWvTtds9ChreHvURQAA3qaZEIToQka8eQhWDT5b6QRjAd00GJBHcwRp")
+const stripe = new Stripe("private key")
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
